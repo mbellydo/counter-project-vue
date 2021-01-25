8d5a122ab028165456e8c6fb867c13eb4da72b49
marc <mbellydo@gmail.com>
2021-01-25 10:12:10 +0100

    done

--- js/app.js
+++ js/app.js
@@ -1,15 +1,43 @@
 const app = Vue.createApp({
     data() {
         return {
-            counter : 0
+            counter: 5,
+            mensajeAlerta: ''
         }
     },
 
-    methods : {
+    methods: {
         updateCounter(num) {
-            this.counter += num
-        },
+            // Transformar this.counter en un entero con el símbolo '+'. Esto nos pasa, porque this.counter ahora es un string, debido a que "traemos" el valor de la propiedad 'value' del input a esta variable. Dicha propiedad 'value' siempre es de tipo string...
+            this.counter = +this.counter + num
+        }
+    },
+
+    computed: {
+        getColorCounter() {
+            if (this.counter>0) {
+                return 'green'
+            }
+
+            return 'yellow'
+        }
+    },
+
+    watch : {
+        counter(value) {
+            console.log("Te vigilo!: " , value)
+            if (value>=20) {
+                this.mensajeAlerta = "Lo siento, no puedo permitir que incrementes más el valor..."
+                this.counter = 20
+
+            }
+
+            else {
+                this.mensajeAlerta = ""
+            }
+        }
     }
+
 })
 
 app.mount('#app')
